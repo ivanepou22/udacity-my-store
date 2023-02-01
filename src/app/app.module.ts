ecd84710d518360922acf3b90cb63694515ebfd5
Ivan Epou <ivanepou92@gmail.com>
2023-02-01 16:32:20 +0300

    Add components, models and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductItemComponent } from './products/product-item/product-item.component';
+import { ProductItemDetailComponent } from './products/product-item-detail/product-item-detail.component';
+import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
+import { CartComponent } from './cart/cart.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    OrderConfirmationComponent,
+    CartComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
